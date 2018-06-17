521c71b68c83805c06efd05e4171a65fc1526186
RayJi0428 <kaikai0428@gmail.com>
2018-06-18 01:39:24 +0800

    commit1
    還缺自動欄寬問題

--- TestExcelNPOI/Form1.Designer.cs
+++ TestExcelNPOI/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -45,10 +47,12 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 9);
+            this.textBox1.Location = new System.Drawing.Point(81, 9);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "2";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
@@ -58,14 +62,36 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 2;
-            this.label1.Text = "保留列數";
+            this.label1.Text = "標題列數";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "姓名所在欄";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -81,6 +107,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- TestExcelNPOI/Form1.cs
+++ TestExcelNPOI/Form1.cs
@@ -18,6 +18,13 @@ namespace TestExcelNPOI
 {
     public partial class Form1 : Form
     {
+        private int keepRow = 2;//保留列數
+
+        private int nameCell = 2;//姓名所在欄
+
+        private Boolean DEBUG = true;
+        private string INPUT_URL = @"input.xls";
+        private string OUTPUT_URL = @"綺綺\#1_#2.xls";
         public Form1()
         {
             InitializeComponent();
@@ -25,118 +32,130 @@ namespace TestExcelNPOI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            FileStream inputStream = new FileStream(@"input.xls", FileMode.Open);
-            HSSFWorkbook inputBook = new HSSFWorkbook(inputStream);
-            HSSFSheet inputSheet = (HSSFSheet)inputBook.GetSheetAt(0);
-
-            //input內的標題列
-            HSSFRow titleRow = (HSSFRow)inputSheet.GetRow(0);
-            int titleCellCount = titleRow.LastCellNum;
-
-            //取得資料(由1開始)
-            int inputRowCount = inputSheet.LastRowNum;
-            for (int i=1; i< inputRowCount; ++i)
+            if (DEBUG)
             {
-                HSSFWorkbook outputBook = new HSSFWorkbook();
-
-                // 在 Excel 工作簿中建立工作表，名稱為 Sheet1
-                HSSFSheet outputSheet = (HSSFSheet)outputBook.CreateSheet("sheet1");
-
-                //取得第N列資料
-                HSSFRow inputRow = (HSSFRow)inputSheet.GetRow(i);
-                string name = "";
-
-                //先寫入標題
-                for (int j = 0; j < titleCellCount; ++j)
-                {
-                    if (j == 0)
-                    {
-                        outputSheet.CreateRow(0);
-                        outputSheet.GetRow(0).Height = titleRow.Height;
-                    }
-
-                    //input.J copy to output.J
-                    ICell curCell = titleRow.GetCell(j);
-                    if (curCell != null)
-                    {
-                        Console.WriteLine("title cell = " + curCell.StringCellValue);
-                        outputSheet.GetRow(0).CreateCell(j).SetCellValue(titleRow.GetCell(j).StringCellValue);
-
-                        HSSFCellStyle newTitleCellStyle = (HSSFCellStyle)outputBook.CreateCellStyle();
-                        newTitleCellStyle.CloneStyleFrom(titleRow.GetCell(j).CellStyle);
-                        outputSheet.GetRow(0).GetCell(j).CellStyle = newTitleCellStyle;
-                    }
-                }
-
-                //寫入資料
-                for (int k=0; k< titleCellCount; ++k)
-                {
-                    if (k == 2)
-                    {
-                        //取得名字
-                        name = inputRow.GetCell(k).StringCellValue;            
-                    }
-
-                    if (k == 0)
-                    {
-                        outputSheet.CreateRow(1);
-                        outputSheet.GetRow(1).Height = inputRow.Height;
-                    }
-
-                    //數字
-                    if (inputRow.GetCell(k).CellType == NPOI.SS.UserModel.CellType.Numeric || inputRow.GetCell(k).CellType == NPOI.SS.UserModel.CellType.Formula)
-                    {
-                        var value = inputRow.GetCell(k).NumericCellValue;
-                        outputSheet.GetRow(1).CreateCell(k).SetCellValue(value);
-                    }
-                    else
-                    {
-                        outputSheet.GetRow(1).CreateCell(k).SetCellValue(inputRow.GetCell(k).StringCellValue);
-                    }
-
-                    HSSFCellStyle newCellStyle = (HSSFCellStyle)outputBook.CreateCellStyle();
-                    newCellStyle.CloneStyleFrom(inputRow.GetCell(k).CellStyle);
-                    outputSheet.GetRow(1).GetCell(k).CellStyle = newCellStyle;
-                    outputSheet.GetRow(1).GetCell(k).CellStyle.WrapText = true;              
-                    outputSheet.AutoSizeColumn(k);
-                    outputSheet.ProtectSheet("123");
-                    
-                    /*
-                    //標題列
-                    if (k == 0)
-                    {
-                        
-                    }
-                    else
-                    {
-                        double value = inputRow.GetCell(j).NumericCellValue;
-                        outputSheet.GetRow(0).CreateCell(j).SetCellType(NPOI.SS.UserModel.CellType.Numeric);
-                        outputSheet.GetRow(0).CreateCell(j).SetCellValue(value);
-                    }
-                    */
-                    
-                }
-
-
-
-                string fileName = @"綺綺\" + name + ".xls";
-                Directory.CreateDirectory(Path.GetDirectoryName(fileName));
-                FileStream outputStream = new FileStream(fileName, FileMode.Create);
-                outputBook.Write(outputStream);
-                outputBook.WriteProtectWorkbook("123", "456");
-                outputStream.Close();
-
+                this.INPUT_URL = "../../../../" + this.INPUT_URL;
+                this.OUTPUT_URL = "../../../../" + this.OUTPUT_URL;
             }
+            try
+            {
+                HSSFWorkbook inputBook = new HSSFWorkbook(new FileStream(INPUT_URL, FileMode.Open));//input不可外流，放在GitHub外層
+                HSSFSheet inputSheet = (HSSFSheet)inputBook.GetSheetAt(0);//目前只取第一分頁sheet0
 
-            MessageBox.Show("END");
+                //取得資料
+                int iNumRow = inputSheet.LastRowNum;
+                for (int i = 0; i < iNumRow; ++i)
+                {
+                    //保留標題列略過
+                    if (i < this.keepRow)
+                        continue;
 
-            /*
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook w = excelApp.Workbooks.Add("AAA");
-            w.Password = "123";
-            w.SaveAs();
-            w.Close();
-            */
+                    HSSFWorkbook outputBook = new HSSFWorkbook();
+                    HSSFSheet outputSheet = (HSSFSheet)outputBook.CreateSheet("sheet1");// 在 Excel 工作簿中建立工作表，名稱為 Sheet1
+
+                    HSSFRow iCurRow = (HSSFRow)inputSheet.GetRow(i);//取得第N列資料
+                    
+
+                    //標題列數(注意:最後一條保留列為資料標題)
+                    int titleCellCount = 0;
+
+                    int ci = 0;
+                    //先寫入保留標題列---------------------------------------------------------------------------------------
+                    for (int ki = 0; ki < this.keepRow; ++ki)
+                    {
+                        //查詢保留列
+                        HSSFRow iTitleRow = (HSSFRow)inputSheet.GetRow(ki);
+                        //保留列有多少欄?
+                        titleCellCount = iTitleRow.LastCellNum;
+                        //建立保留列
+                        HSSFRow oTitleRow = (HSSFRow)outputSheet.CreateRow(ki);
+                        //設定同等列高
+                        oTitleRow.Height = iTitleRow.Height;
+
+                        for (ci = 0; ci < titleCellCount; ++ci)
+                        {
+                            //input.J copy to output.J
+                            ICell inTItleCell = iTitleRow.GetCell(ci);
+
+                            if (inTItleCell != null)
+                            {
+                                ICell oTitleCell = oTitleRow.CreateCell(ci);
+                                oTitleCell.SetCellValue(inTItleCell.StringCellValue);
+
+                                //複製欄格式
+                                HSSFCellStyle newTitleCellStyle = (HSSFCellStyle)outputBook.CreateCellStyle();
+                                newTitleCellStyle.CloneStyleFrom(inTItleCell.CellStyle);
+                                oTitleCell.CellStyle = newTitleCellStyle;
+                            }
+                        }
+                    }
+
+                    //寫入資料---------------------------------------------------------------------------------------
+                    //建立員工資料列
+                    string name = "temp";
+                    string apartment = "temp";
+                    HSSFRow oDataRow = (HSSFRow)outputSheet.CreateRow(this.keepRow);
+                    oDataRow.Height = iCurRow.Height;
+                    for (ci = 0; ci < titleCellCount; ++ci)
+                    {
+                        ICell inDataCell = iCurRow.GetCell(ci);
+
+                        //取得名字
+                        if (ci == this.nameCell)
+                        {
+                            name = inDataCell.StringCellValue;
+                        }
+                        if (ci == this.nameCell + 1)
+                        {
+                            apartment = inDataCell.StringCellValue;
+                        }
+
+                        ICell oDataCell = oDataRow.CreateCell(ci);
+
+                        //到職日特別處理
+                        if (ci == 0)
+                        {
+                            var value = inDataCell.DateCellValue;
+                            oDataCell.SetCellValue(value.ToShortDateString());//轉換為2014/12/19
+                        }
+                        //數字
+                        else if (inDataCell.CellType == NPOI.SS.UserModel.CellType.Numeric || inDataCell.CellType == NPOI.SS.UserModel.CellType.Formula)
+                        {
+                            var value = inDataCell.NumericCellValue;
+                            oDataCell.SetCellValue(value);
+                        }
+                        else
+                        {
+                            oDataCell.SetCellValue(inDataCell.StringCellValue);
+                        }
+
+                        HSSFCellStyle newCellStyle = (HSSFCellStyle)outputBook.CreateCellStyle();
+                        newCellStyle.CloneStyleFrom(inDataCell.CellStyle);
+                        oDataCell.CellStyle = newCellStyle;
+                        outputSheet.AutoSizeColumn(ci);
+                        //oDataCell.CellStyle.WrapText = true;
+                    }
+
+                    outputSheet.AutoSizeColumn(9);
+
+                    //output不可外流，放在GitHub外層
+                    string fileName = OUTPUT_URL;
+                    fileName = fileName.Replace("#1", apartment);
+                    fileName = fileName.Replace("#2", name);
+                    Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+                    FileStream outputStream = new FileStream(fileName, FileMode.Create);
+                    outputBook.Write(outputStream);
+                    outputBook.WriteProtectWorkbook("123", "456");
+                    outputStream.Close();
+
+                }
+
+                MessageBox.Show("薪資拆分成功!");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("異常錯誤 " + error.Message);
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -146,7 +165,23 @@ namespace TestExcelNPOI
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            this.keepRow = Int32.Parse(textBox1.Text);
+        }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private  void backup()
+        {
+            /*
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook w = excelApp.Workbooks.Add("AAA");
+            w.Password = "123";
+            w.SaveAs();
+            w.Close();
+            */
         }
     }
 }
